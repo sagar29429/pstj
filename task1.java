8f3f430518b8cca08bc3af24c23e844747b00363
Vidya sagar <sagarpraharaju@gmail.com>
2026-08-12 11:03:58 +0530

    first commit

---
+++ task1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] salaries = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            salaries[i] = sc.nextInt();
+        }
+
+        Arrays.stream(salaries)
+              .map(salary -> salary + salary / 10)
+              .forEach(salary -> System.out.print(salary + " "));
+    }
+}
